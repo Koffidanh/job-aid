f1b5e6d98984c0dff89c1e9ac03a0f92e0a561ff
kayvonk <kayvonk@gmail.com>
2021-03-26 20:22:20 -0600

    implementing routes for handlebars

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ var session = require("express-session");
 var passport = require("./config/passport");
 // Set Handlebars.
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
+require('dotenv').config();
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(bodyParser.json());
+
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
@@ -22,16 +28,24 @@ app.use(passport.session());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 // Requiring our routes
+
+// const routes = require('./routes/customer-api-routes')
+// app.use('/customer', routes);
+
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+// require("./routes/customer-api-routes.js")(app);
+
 // Import routes and give the server access to them.
-const customerRoutes = require('./controllers/customer_controller.js');
-app.use(customerRoutes);
+// const customerRoutes = require('./controllers/customer_controller.js');
+// app.use(customerRoutes);
+
+
 //need to create some for the login and others
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
