891474f5b16d937e0354c0f081365d28c06b9bd0
kayvonk <kayvonk@gmail.com>
2021-03-29 21:32:02 -0600

    Our work from today getting dot env to function

--- models/index.js
+++ models/index.js
@@ -7,11 +7,15 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
+require('dotenv').config();
+
+let sequelize;
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  console.log(process.env[config.use_env_variable])
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
