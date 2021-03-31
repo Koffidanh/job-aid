33f9126c90cb133d2d10a5802ea6882f193131e0
jlbaldree <jon.baldree@gmail.com>
2021-03-31 00:10:58 -0600

    view jobs retrieves job related to customer ID

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -94,13 +94,24 @@ module.exports = function (app) {
     console.log(req.params)
     const id = Number.parseInt(req.params.id, 10)
     console.log(id)
-    const jobDetails = await db.JobType.findByPk(id)
+    const jobDetails = await db.JobType.findOne({
+      where: {
+      CustomerId: id
+      }
+    });
     console.log(jobDetails)
-    res.render('job', { JobType: jobDetails.get({ plain: true }) });
+    if (jobDetails==false) {
+      alert('No jobs exist for customer');
+    } else {
+    res.render('job', { JobType: jobDetails.get({ plain: true })
+    }) 
+    }
+  
+  });
 
 
-  })
-
+  
+  
 
   //   app.get('/search:firstName'), (req, res) =>
   //     db.Customers.findAll({
